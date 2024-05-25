dcb62a01c82ba7f90bb830897fb80648c9fadbbc
Sulaiman Nadeem <sunnad99@gmail.com>
2024-05-26 01:16:54 +0500

    feat: Add UnauthorizedResponse model for handling unauthorized requests

--- routes/route_params/generic.py
+++ routes/route_params/generic.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class BadRequestResponse(BaseModel):
     error: str
     status_code: int = 400
+
+
+class UnauthorizedResponse(BaseModel):
+    error: str
+    status_code: int = 401

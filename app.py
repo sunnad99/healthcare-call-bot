c144972db05c340219d83f46c47e4155ab8689cc
Sulaiman Nadeem <sunnad99@gmail.com>
2024-05-31 20:01:05 +0500

    feat: Add CORS middleware to allow cross-origin requests

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes import get_surveyor, get_survey, get_calls, get_questionnaire, submit_questionnaire
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    max_age=3600,
+)
 
 app.include_router(get_surveyor.router)
 app.include_router(get_survey.router)
@@ -20,7 +30,3 @@ async def health():
 @app.get("/")
 def read_root():
     return {"message": "Welcome to CG-OSMS API."}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, port="8000")
